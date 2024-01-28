61fbb977b556551c244bb576b68d3fc9b15a6a23
Thasnima Begum <thasnima@hotmail.co.uk>
2024-01-28 21:48:57 +0000

    delete artist complete

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -11,4 +11,92 @@ const postArtist = async (req, res) => {
   }
 }
 
-module.exports = {postArtist}
+const getArtists = async (_, res)=>{
+  try{
+    const { rows } = await db.query("SELECT * FROM artists")
+    res.status(200).json(rows)
+  }
+  catch(error){
+    res.status(500).json(error)
+  }
+}
+
+const getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rows: [artist] } = await db.query("SELECT * FROM artists WHERE id = $1", [id]) 
+    
+    if (!artist) {
+      return res.status(404).json({message: `artist ${id} does not exist`})
+    }
+
+    res.status(200).json(artist[0])
+  } catch (error){
+    res.status(500).json(error.message)
+  }
+}
+
+const updateArtistById = async (req, res) => {
+  try {
+    const {name, genre} = req.body
+    const { id } = req.params;
+    const {rows: [artist]} = await db.query("UPDATE artists SET name = $1, genre=$2 WHERE id = $3 RETURNING *", [name, genre, id])
+
+    if (!artist) {
+      return res.status(404).json({message: `artist ${id} does not exist`})
+    }
+
+    res.status(200).json(artist)
+  } catch(error){
+    res.status(500).json(error)
+  }
+}
+
+const patchArtistById = async (req, res) => {
+    const {name, genre} = req.body
+    const {id} = req.params
+    let query, params
+    if(name && genre){
+      query = "UPDATE artists SET name=$1, genre=$2 WHERE id=$3 RETURNING *"
+      params = [name, genre, id]
+    }
+    else if (name){
+      query = "UPDATE artists SET name=$1 WHERE id=$2 RETURNING *"
+      params = [name, id]
+    }
+    else if (genre){
+      query = "UPDATE artists SET genre=$1 WHERE id=$2 RETURNING *"
+      params = [genre, id]
+    }
+
+try {
+    const {rows: [artist]} = await db.query(query, params)
+    if (!artist) {
+      return res.status(404).json({message: `artist ${id} does not exist`})
+    }
+    res.status(200).json(artist)
+
+  } catch(error){
+    res.status(500).json(error)
+  }
+}
+
+const deleteArtistById = async (req, res) => {
+  try {
+    const {id} = req.params
+    const {rows:[artist]} = await db.query("DELETE FROM artists WHERE id=$1 RETURNING *", [id])
+
+    if (!artist) {
+      return res.status(404).json({message: `artist ${id} does not exist`})
+    }
+
+    res.status(200).json(artist)
+
+  } catch(error) {
+    res.status(500).json(error)
+  }
+}
+
+
+
+module.exports = {postArtist, getArtists, getArtistById, updateArtistById, patchArtistById, deleteArtistById}
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const {postArtist} = require('../controllers/artist')
+const {postArtist, getArtists, getArtistById, updateArtistById, patchArtistById, deleteArtistById} = require('../controllers/artist')
 const artistRouter = express.Router()
 
 artistRouter.post('/artists', postArtist)
+
+artistRouter.get('/artists', getArtists)
+
+artistRouter.get('/artists/:id', getArtistById)
+
+artistRouter.put('/artists/:id', updateArtistById)
+
+artistRouter.patch('/artists/:id', patchArtistById)
+
+artistRouter.delete('/artists/:id', deleteArtistById)
 
 module.exports = artistRouter
