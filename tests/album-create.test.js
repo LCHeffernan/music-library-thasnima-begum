6a7695a79e12e82a0a2f77990cdac684910572b3
Thasnima Begum <thasnima@hotmail.co.uk>
2024-02-05 21:01:40 +0000

    album create test done

--- tests/album-create.test.js
+++ tests/album-create.test.js
@@ -4,24 +4,31 @@ const db = require('../src/db')
 const app = require('../src/app')
 
 describe('create album', () => {
-  describe('/artists/:id/albums', () => {
-    describe('POST', () => {
-      it('creates a new album in the database', async () => {
-        const { status, body } = await request(app).post('/artists/:id/album').send({
-          name: 'Tame Impala',
-          genre: 'rock',
+  let artist;
+  beforeEach( async ()=>{
+    const {rows} = await db.query('INSERT INTO artists (name, genre) VALUES ($1, $2) RETURNING *', ['Taylor Swift', 'pop'])
+    artist = rows[0]
+  })
+
+  describe('POST /artists/:id/albums', () => {
+   it('creates a new album in the database', async () => {
+        const id = artist.id
+        const { status, body } = await request(app).post(`/artists/${id}/albums`).send({
+          name: '1984',
+          year: '2021',
         });
 
         expect(status).to.equal(201);
-        expect(body.name).to.equal('Tame Impala');
-        expect(body.genre).to.equal('rock');
+        expect(body.name).to.equal('1984');
+        expect(body.year).to.equal(2021);
 
         const {
-          rows: [artistData],
-        } = await db.query(`SELECT * FROM Artists WHERE id = ${body.id}`);
-        expect(artistData.name).to.equal('Tame Impala');
-        expect(artistData.genre).to.equal('rock');
-      });
+          rows: [albumData],
+        } = await db.query(`SELECT * FROM albums WHERE id = ${body.id}`);
+        expect(albumData.name).to.equal('1984');
+        expect(albumData.year).to.equal(2021);
+        expect(albumData.artistid).to.equal(id)
     });
   });
 });
+
